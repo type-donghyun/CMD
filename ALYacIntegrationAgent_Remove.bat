f75b5c0ec922e2d701a2bbacc1c256fa95b65a9e
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-11-24 12:35:46 +0900

    Update ALYacIntegrationAgent_Remove.bat

--- ALYacIntegrationAgent_Remove.bat
+++ ALYacIntegrationAgent_Remove.bat
@@ -28,13 +28,13 @@ IF %errorlevel% == 1 (
 	goto :Endlevel
 )
 
-SET directory1=%ProgramFiles%\ESTsoft\ALYac
-SET directory2=%ProgramFiles%\ESTsoft\ALYacIntegrationAgent
-SET directory3=%ProgramFiles%\ESTsoft\ASM
-SET directory4=%ProgramData%\ESTsoft\ALYac
-SET directory5=%ProgramData%\ESTsoft\ALYacIntegrationAgent
-SET directory6=%ProgramData%\ESTsoft\ASM
-SET directory7=%ProgramData%\Microsoft\Windows\"Start menu"\Programs\이스트소프트
+SET directory1 = %ProgramFiles%\ESTsoft\ALYac
+SET directory2 = %ProgramFiles%\ESTsoft\ALYacIntegrationAgent
+SET directory3 = %ProgramFiles%\ESTsoft\ASM
+SET directory4 = %ProgramData%\ESTsoft\ALYac
+SET directory5 = %ProgramData%\ESTsoft\ALYacIntegrationAgent
+SET directory6 = %ProgramData%\ESTsoft\ASM
+SET directory7 = %ProgramData%\Microsoft\Windows\"Start menu"\Programs\이스트소프트
 
 ECHO 디렉토리 제거 시작
 ECHO.
@@ -52,7 +52,7 @@ ECHO 디렉토리 제거 완료
 TIMEOUT /t 1 > nul
 CLS
 
-SET file1=%ProgramData%\Microsoft\Windows\"Start menu"\알약.lnk
+SET file1 = %ProgramData%\Microsoft\Windows\"Start menu"\알약.lnk
 
 ECHO 파일 제거 시작
 ECHO.
@@ -70,21 +70,21 @@ ECHO 파일 제거 완료
 TIMEOUT /t 1 > nul
 CLS
 
-SET registry1=HKLM\SOFTWARE\ESTsoft\ALYac
-SET registry2=HKLM\SOFTWARE\ESTsoft\ALYacIntegrationAgent
-SET registry3=HKLM\SOFTWARE\ESTsoft\ASM
+SET registry1 = HKLM\SOFTWARE\ESTsoft\ALYac
+SET registry2 = HKLM\SOFTWARE\ESTsoft\ALYacIntegrationAgent
+SET registry3 = HKLM\SOFTWARE\ESTsoft\ASM
 
 ECHO 레지스트리 제거 시작
 ECHO.
-SET /a count=0
+SET /a count = 0
 
 :regdel
 SET /a count += 1
 REG QUERY registry%count% /f 2> nul
-IF %errorlevel%==0 (
+IF %errorlevel% == 0 (
 	REG DELETE registry%count% /f 2> nul
 	ECHO %count%번째 레지스트리를 제거
-) ELSE IF %errorlevel%==1 (
+) ELSE IF %errorlevel% == 1 (
 	ECHO %count%번째 레지스트리를 발견하지 못 함
 )
 
