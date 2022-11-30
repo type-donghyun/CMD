8d6f591544693729faccb0fcf925454cfe6b88ab
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-11-30 13:17:30 +0900

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,20 +1,18 @@
 @ECHO OFF
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-IF %errorlevel% neq 0 (
-	GOTO UACPrompt
-) ELSE (
-	GOTO gotAdmin
-)
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
 :UACPrompt
-	ECHO SET UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-	SET params = %*:"=""
-	ECHO UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-	"%temp%\getadmin.vbs"
-	REM DEL "%temp%\getadmin.vbs"
-	EXIT /b
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    rem del "%temp%\getadmin.vbs"
+    exit /B
 :gotAdmin
-PUSHD "%CD%"
-	CD /d "%~dp0"
+pushd "%CD%"
+    CD /D "%~dp0"
 
 CHCP 65001 > nul
 
@@ -23,8 +21,8 @@ ECHO 다시 시작 시, 실행되고 있는 모든 프로그램이 강제로 종
 ECHO 다시 시작 후, 바탕 화면의 {저를 실행해주세요!}를 실행해주세요.
 ECHO 안전모드로 변경 후 다시 시작하시겠습니까?
 CHOICE
-CLS
 
+CLS
 IF %errorlevel% equ 1 (
 	BCDEDIT /set {current} safeboot minimal > nul
 	COPY bat\ALYacIntegrationAgent_Remove.bat %UserProfile%\desktop\"저를 실행해주세요!".bat > nul
