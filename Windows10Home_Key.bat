7af9ca6761f5be81a56bf391683a06071ef2abdb
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-11-30 12:03:23 +0900

    Update Windows10Home_Key.bat

--- Windows10Home_Key.bat
+++ Windows10Home_Key.bat
@@ -1,18 +1,20 @@
 @ECHO OFF
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-if '%errorlevel%' NEQ '0' (
-    goto UACPrompt
-) else ( goto gotAdmin )
+IF %errorlevel% neq 0 (
+	GOTO UACPrompt
+) ELSE (
+	GOTO gotAdmin
+)
 :UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-    "%temp%\getadmin.vbs"
-    rem del "%temp%\getadmin.vbs"
-    exit /B
+	ECHO SET UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+	SET params = %*:"=""
+	ECHO UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+	"%temp%\getadmin.vbs"
+	REM DEL "%temp%\getadmin.vbs"
+	EXIT /b
 :gotAdmin
-pushd "%CD%"
-    CD /D "%~dp0"
+PUSHD "%CD%"
+	CD /d "%~dp0"
 
 CHCP 65001 > nul
 ECHO Windows 10 Home 정품 활성화 중...
