9b035995e4f84593867167af488eb6bb7dc42b71
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-12-01 09:52:40 +0900

    Update and rename ProcessorPerformanceBoostMode_Enable.bat to Settings/ProcessorPerformanceBoostMode_Enable.bat

--- Settings/ProcessorPerformanceBoostMode_Enable.bat
+++ Settings/ProcessorPerformanceBoostMode_Enable.bat
@@ -16,7 +16,7 @@ IF %errorlevel% neq 0 (
 PUSHD "%CD%"
 	CD /d "%~dp0"
 
-REG ADD HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\be337238-0d82-4146-a960-4f3749d470c7 /v Attributes /t REG_DWORD /d 2 /f > nul
+REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\be337238-0d82-4146-a960-4f3749d470c7" /v "Attributes" /t REG_DWORD /d 2 /f > nul
 CHCP 65001 > nul
 ECHO 프로세서 성능 강화 모드 속성을 노출시켰습니다.
 TIMEOUT /t 3 > nul
