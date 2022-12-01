d6c862f02c909ba9e25a61cc0790213d263c8367
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-12-01 09:53:39 +0900

    Update ExcludeWUDriversInQualityUpdate_Enable.bat

--- Settings/ExcludeWUDriversInQualityUpdate_Enable.bat
+++ Settings/ExcludeWUDriversInQualityUpdate_Enable.bat
@@ -16,7 +16,7 @@ IF %errorlevel% neq 0 (
 PUSHD "%CD%"
 	CD /d "%~dp0"
 
-REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /v ExcludeWUDriversInQualityUpdate /t REG_DWORD /d 1 /f > nul
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /v "ExcludeWUDriversInQualityUpdate" /t REG_DWORD /d 1 /f > nul
 GPUPDATE /force
 CHCP 65001 > nul
 ECHO Windows 업데이트에 드라이버를 포함하지 않음 옵션을 사용으로 변경했습니다.
