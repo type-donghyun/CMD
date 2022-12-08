e101664db60d8accf19768bc9778b6ca5dfcfca4
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-12-08 10:37:37 +0900

    Update Office365_install.bat

--- Install/Office_Install/Office365_install.bat
+++ Install/Office_Install/Office365_install.bat
@@ -21,12 +21,13 @@ CHCP 65001 > nul
 ECHO Office 365 설치
 ECHO 포함된 제품은 Excel, Word, Powerpoint입니다.
 ECHO 설치에는 인터넷 연결이 필요하며 시간이 다소 소요될 수 있습니다.
-ECHO 아무 버튼이나 누르면 설치가 시작됩니다.
-PAUSE > nul
+ECHO 10초 후, 설치가 시작됩니다.
+TIMEOUT /t 10 > nul
 
+CLS
 CD data > nul
 ECHO 설치 파일 다운로드 중...
 setup.exe /download configuration.xml
 CLS
-ECHO 설치 중...
+ECHO 설치 진행 중...
 setup.exe /configure configuration.xml
