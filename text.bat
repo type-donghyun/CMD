42ec36299e0e9cec4a006f09dd5d8d7367b676b5
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-12-13 00:43:12 +0900

    Update text.bat

--- text.bat
+++ text.bat
@@ -1,19 +1,72 @@
 @ECHO OFF
+::================================================================================관리자 권한 요청
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+IF %errorlevel% neq 0 (
+	GOTO UACPrompt
+) ELSE (
+	GOTO gotAdmin
+)
+:UACPrompt
+	ECHO SET UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+	SET params = %*:"=""
+	ECHO UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+	"%temp%\getadmin.vbs"
+	REM DEL "%temp%\getadmin.vbs"
+	EXIT /b
+:gotAdmin
+PUSHD "%CD%"
+	CD /d "%~dp0"
+::====================================================================================================
+
+::================================================================================ECHO 색상 설정
+SET _elev=
+IF /i "%~1"=="-el" SET _elev=1
+FOR /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
+SET "_null=1>nul 2>nul"
+SET "_psc=powershell"
+SET "EchoBlack=%_psc% write-host -back DarkGray -fore Black"
+SET "EchoBlue=%_psc% write-host -back Black -fore DarkBlue"
+SET "EchoGreen=%_psc% write-host -back Black -fore Darkgreen"
+SET "EchoCyan=%_psc% write-host -back Black -fore DarkCyan"
+SET "EchoRed=%_psc% write-host -back Black -fore DarkRed"
+SET "EchoPurple=%_psc% write-host -back Black -fore DarkMagenta"
+SET "EchoYellow=%_psc% write-host -back Black -fore DarkYellow"
+SET "EchoWhite=%_psc% write-host -back Black -fore Gray"
+SET "EchoGray=%_psc% write-host -back Black -fore DarkGray"
+SET "EchoLightBlue=%_psc% write-host -back Black -fore Blue"
+SET "EchoLightGreen=%_psc% write-host -back Black -fore Green"
+SET "EchoLightCyan=%_psc% write-host -back Black -fore Cyan"
+SET "EchoLightRed=%_psc% write-host -back Black -fore Red"
+SET "EchoLightPurple=%_psc% write-host -back Black -fore Magenta"
+SET "EchoLightYellow=%_psc% write-host -back Black -fore Yellow"
+SET "EchoBrightWhite=%_psc% write-host -back Black -fore White"
+SET "ErrLine=echo: & %EchoRed% ==== ERROR ==== &echo:"
+::====================================================================================================
+
 CHCP 65001 > nul
+
 FOR /f "tokens=4-6" %%a in ('systeminfo') do (
-	IF %%a equ Windows (
-		SET hello=%%a %%b %%c
+	IF %%a==Windows (
+		ECHO %%a %%b %%c> %temp%\test.txt
 	)
 )
 
-ECHO 설치된 윈도우는 %hello%입니다
-SET hello=%%hello
+SET a= <%temp%\test.txt
+ECHO %a%
+TIMEOUT /t 2 > nul
 
-IF %%hello equ "Windows 10 Pro" (
-	echo 프로
-) ELSE IF %%hello equ "Windows 10 Home" (
-	echo 홈
+IF  "%a%" == "Windows 10 Pro" (
+	%echogreen% Windows 10 Pro가 감지되었습니다.
+) ELSE IF "%a%" == "Windows 10 Home" (
+	%echogreen% Windows 10 Home이 감지되었습니다.
 ) ELSE (
-	ECHO 불가능한 Windows 버전: hello%
+	%echored% 불가능한 Windows 버전: %a%
+	ECHO %a%
+	ECHO 작업을 종료합니다.
+	TIMEOUT /t 3 > nul
+	GOTO :workend
 )
-TIMEOUT /t 10 > nul
+
+:workend
+DEL %temp%\test.txt
+EXIT /b
