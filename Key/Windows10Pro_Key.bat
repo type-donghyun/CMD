ec97c7b73fea03238297849a65c7d2b28f782c5a
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2022-12-12 11:02:40 +0900

    Update Windows10Pro_Key.bat

--- Key/Windows10Pro_Key.bat
+++ Key/Windows10Pro_Key.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+::================================================================================관리자 권한 요청
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 IF %errorlevel% neq 0 (
 	GOTO UACPrompt
@@ -15,13 +16,40 @@ IF %errorlevel% neq 0 (
 :gotAdmin
 PUSHD "%CD%"
 	CD /d "%~dp0"
+::====================================================================================================
+
+::================================================================================ECHO 색상 설정
+SET _elev=
+IF /i "%~1"=="-el" SET _elev=1
+FOR /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
+SET "_null=1>nul 2>nul"
+SET "_psc=powershell"
+SET "EchoBlack=%_psc% write-host -back DarkGray -fore Black"
+SET "EchoBlue=%_psc% write-host -back Black -fore DarkBlue"
+SET "EchoGreen=%_psc% write-host -back Black -fore Darkgreen"
+SET "EchoCyan=%_psc% write-host -back Black -fore DarkCyan"
+SET "EchoRed=%_psc% write-host -back Black -fore DarkRed"
+SET "EchoPurple=%_psc% write-host -back Black -fore DarkMagenta"
+SET "EchoYellow=%_psc% write-host -back Black -fore DarkYellow"
+SET "EchoWhite=%_psc% write-host -back Black -fore Gray"
+SET "EchoGray=%_psc% write-host -back Black -fore DarkGray"
+SET "EchoLightBlue=%_psc% write-host -back Black -fore Blue"
+SET "EchoLightGreen=%_psc% write-host -back Black -fore Green"
+SET "EchoLightCyan=%_psc% write-host -back Black -fore Cyan"
+SET "EchoLightRed=%_psc% write-host -back Black -fore Red"
+SET "EchoLightPurple=%_psc% write-host -back Black -fore Magenta"
+SET "EchoLightYellow=%_psc% write-host -back Black -fore Yellow"
+SET "EchoBrightWhite=%_psc% write-host -back Black -fore White"
+SET "ErrLine=echo: & %EchoRed% ==== ERROR ==== &echo:"
+::====================================================================================================
 
 CHCP 65001 > nul
-ECHO Windows 10 Pro 정품 활성화 중...
+
+%echogreen% Windows 10 Pro 정품 활성화 중...
 ECHO 정품 인증은 6개월 후, 만료됩니다.
 
-SLMGR /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-SLMGR /skms kms.digiboy.ir
-SLMGR /ato
-SLMGR /xpr
-SLMGR /dlv
+SLMGR /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX > nul
+SLMGR /skms kms.digiboy.ir > nul
+SLMGR /ato > nul
+SLMGR /xpr > nul
+SLMGR /dlv > nul
